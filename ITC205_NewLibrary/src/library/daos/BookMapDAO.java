0e8857eee7d368db0040fe9f12094ca315fe9722
Gurjit4800 <gurjit1993singh@gmail.com>
2016-09-27 12:08:50 +1000

    1

--- ITC205_NewLibrary/src/library/daos/BookMapDAO.java
+++ ITC205_NewLibrary/src/library/daos/BookMapDAO.java
@@ -25,7 +25,7 @@ public class BookMapDAO implements IBookDAO {
 		bookMap = new HashMap<Integer, IBook>();
 	}
 	/
-//115572
+//1155725
 	public BookMapDAO(IBookHelper helper, Map<Integer, IBook> bookMap) {
 		this(helper);
 		if (helper == null ) {
